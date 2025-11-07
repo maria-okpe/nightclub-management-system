e0f4b9dd4070c5cf6cef19df4834f2a989e20e17
maria-okpe <mariaokpe12@gmail.com>
2025-11-07 08:22:01 -0800

    Add passing unit test

---
+++ src/test/java/com/nightclub/nightclub_management/SampleTest.java
@@ -0,0 +1,12 @@
+package com.nightclub.nightclub_management;
+
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SampleTest {
+    @Test
+    void testAddition() {
+        assertEquals(5, 2 + 3); // should pass
+    }
+}
