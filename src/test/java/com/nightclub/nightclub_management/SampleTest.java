559a8f7cafc89ebfea26cd9bcb7a7c061aa68f47
maria-okpe <mariaokpe12@gmail.com>
2025-11-07 14:38:54 -0800

    remove extra space

--- src/test/java/com/nightclub/nightclub_management/SampleTest.java
+++ src/test/java/com/nightclub/nightclub_management/SampleTest.java
@@ -7,13 +7,13 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 class SampleTest {
     @Test
     void testAddition() {
-        assertEquals(5, 2 + 3); // should pass
+        assertEquals(5, 2 + 3);
     }
 
     @Test
     void testAdditionShouldFail() {
         int result = 2 + 3;
-        assertEquals(6, result); // ❌ Intentionally wrong expected value
+        assertEquals(6, result);
     }
 
     @Test
